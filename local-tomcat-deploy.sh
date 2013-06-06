65dcb7d0b2975c111b12c01209fac2e55c23c534
Albert De La Fuente <vonpupp@gmail.com>
2013-06-06 11:06:47 -0300

    piloto @vonpupp copilotos @walmeida @renatominami - Atualizacao com os passos faltantes

--- local-tomcat-deploy.sh
+++ local-tomcat-deploy.sh
@@ -1,22 +1,26 @@
 TOMCATROOT="$HOME/apache-tomcat-7.0.39"
-TIKTAKROOT="$HOME/labXP/tiktak/"
-DASHBOARDROOT="$TIKTAKROOT/tiktak-dashboard/core/"
+TIKTAKROOT="$HOME/labXP/tiktak"
+DASHBOARDROOT="$TIKTAKROOT/tiktak-dashboard"
 
-cd $DASHBOARDROOT
+# 1
+cd $DASHBOARDROOT/core/
 mvn clean install
 mvn refdb:ref -Ph2
 
 cd ..
 mvn clean install
 
+# 2
 mkdir $TOMCATROOT/tiktak/
 
+# 3
 cp -av $HOME/.m2/repository/com/h2database/h2/1.1.109/h2-1.1.109.jar $TOMCATROOT/lib/
 
+# 4
 mkdir $TOMCATROOT/lib/tiktak.jar/
+cp -av $DASHBOARDROOT/web/src/test/resources/deploy/* $TOMCATROOT/lib/tiktak.jar/
 
-cp -av $HOME/labXP/tiktak/tiktak-dashboard/web/src/test/resources/deploy/* $TOMCATROOT/lib/tiktak.jar/
-
+# 5
 cat <<EOF >> $TOMCATROOT/lib/tiktak.jar/application-config.properties
 
 #conf para H2
@@ -27,6 +31,11 @@ datasource.password=a
 database.hibernate.dialect=jmine.tec.environment.db.dialect.H2SequenceDialect
 EOF
 
-cp -av $DASHBOARDROOT/target/loadeddb* $TOMCATROOT/tiktak/
+# 6
+cp -av $DASHBOARDROOT/core/target/loadeddb* $TOMCATROOT/tiktak/
 
-cp -av
+# 7
+cp -av $DASHBOARDROOT/web/target/tiktak-dashboard.war $TOMCATROOT/webapps
+
+# 8
+cp -av $HOME/.m2/repository/jmine/tec/jmine-tec-persist/2.10.2-RC1/jmine-tec-persist-2.10.2-RC1-tests.jar $TOMCAT/webapps/tiktak-dashboard/WEB-INF/lib
