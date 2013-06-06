15907a3dbf96e3b9b65a76188be17e71bdf1305a
Albert De La Fuente <vonpupp@gmail.com>
2013-06-06 11:42:39 -0300

    piloto @vonpupp copilotos @walmeida @renatominami - Modificacoes ultra mega master experimentais ao cubo

--- local-tomcat-deploy.sh
+++ local-tomcat-deploy.sh
@@ -2,25 +2,27 @@ TOMCATROOT="$HOME/apache-tomcat-7.0.39"
 TIKTAKROOT="$HOME/labXP/tiktak"
 DASHBOARDROOT="$TIKTAKROOT/tiktak-dashboard"
 
-# 1
+$TOMCATROOT/bin/shutdown.sh
+
+echo "#1"
 cd $DASHBOARDROOT/core/
 mvn clean install
 mvn refdb:ref -Ph2
 
 cd ..
-mvn clean install
+#mvn clean install
 
-# 2
-mkdir $TOMCATROOT/tiktak/
+echo "#2"
+mkdir $TOMCATROOT/bin/tiktak/
 
-# 3
+echo "#3"
 cp -av $HOME/.m2/repository/com/h2database/h2/1.1.109/h2-1.1.109.jar $TOMCATROOT/lib/
 
-# 4
+echo "#4"
 mkdir $TOMCATROOT/lib/tiktak.jar/
 cp -av $DASHBOARDROOT/web/src/test/resources/deploy/* $TOMCATROOT/lib/tiktak.jar/
 
-# 5
+echo "#5"
 cat <<EOF >> $TOMCATROOT/lib/tiktak.jar/application-config.properties
 
 #conf para H2
@@ -31,11 +33,19 @@ datasource.password=a
 database.hibernate.dialect=jmine.tec.environment.db.dialect.H2SequenceDialect
 EOF
 
-# 6
-cp -av $DASHBOARDROOT/core/target/loadeddb* $TOMCATROOT/tiktak/
+echo "#6"
+cp -av $DASHBOARDROOT/core/target/loadeddb* $TOMCATROOT/bin/tiktak/
 
-# 7
+# Experimental
+cd $DASHBOARDROOT
+#mvn clean install
+mvn install
+
+echo "#7"
 cp -av $DASHBOARDROOT/web/target/tiktak-dashboard.war $TOMCATROOT/webapps
 
-# 8
+$TOMCATROOT/bin/startup.sh
+sleep 15
+
+echo "#8"
 cp -av $HOME/.m2/repository/jmine/tec/jmine-tec-persist/2.10.2-RC1/jmine-tec-persist-2.10.2-RC1-tests.jar $TOMCAT/webapps/tiktak-dashboard/WEB-INF/lib/
