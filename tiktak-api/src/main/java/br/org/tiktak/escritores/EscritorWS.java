3c925cce4da592a5a5871a0c53fdc86648181388
Wallace <wallace.almeida89@gmail.com>
2013-06-27 11:35:16 -0300

    piloto @vonpupp copiloto @walmeida - Adicionado teste simples para verificar que a serializacao dos dados por rede via POST funciona

--- tiktak-api/src/main/java/br/org/tiktak/escritores/EscritorWS.java
+++ tiktak-api/src/main/java/br/org/tiktak/escritores/EscritorWS.java
@@ -10,14 +10,14 @@ import java.net.URLEncoder;
 public class EscritorWS extends Escritor{
 
 	@Override
-	public void escreve(final String usuario, final String nomeDoEvento, final String caminhoDeEscrita) throws Exception {
+	public void escreve(final String usuario, final String nomeDoEvento, final String host) throws Exception {
 	    String data = URLEncoder.encode(usuario, "UTF-8") + "=" + URLEncoder.encode(nomeDoEvento, "UTF-8");
 	    
-	    Socket socket = new Socket("127.0.0.1", 8080);
+	    Socket socket = new Socket(host, 8080);
 
 	    String path = "/servlet";
-	    BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
-	    wr.write("POST " + path + " HTTP/1.0\r\n");
+	    BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+	    wr.write("POST " + path + " HTTP/1.1\r\n");
 	    wr.write("Content-Length: " + data.length() + "\r\n");
 	    wr.write("Content-Type: application/x-www-form-urlencoded\r\n");
 	    wr.write("\r\n");
