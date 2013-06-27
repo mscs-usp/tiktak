639181195ff9a669076e458cb942713208e2c0b3
Roberto Rodrigues <me@rrodrigues.net>
2013-06-27 15:47:21 -0300

    piloto @RobertoRodrigues copiloto @renatominami : criação de javadoc e mudança do pom para que o javadoc gerado fique disponível no jar e na importação da API pelo maven

--- tiktak-api/src/main/java/br/org/tiktak/core/TikTak.java
+++ tiktak-api/src/main/java/br/org/tiktak/core/TikTak.java
@@ -10,31 +10,52 @@ import br.org.tiktak.escritores.Escritor;
 import br.org.tiktak.escritores.EscritorArquivo;
 import br.org.tiktak.escritores.EscritorWS;
 
+/**
+ * TikTak API - API to collect feedback on the use of source code.
+ * 
+ * @since 2013-06
+ * @version 0.3
+ * @author Albert De La Fuente, Claynon Souza, Julian Monteiro, Julien Renaut,
+ *         Leonardo Oliveira, Renan Melo, Renato Minami, Roberto Rodrigues,
+ *         Wallace Almeida
+ * @license LGPL, for more visit: <a href =
+ *          "http://www.gnu.org/copyleft/lesser.html"
+ *          >http://www.gnu.org/copyleft/lesser.html</a>
+ * */
 public class TikTak {
-	private String caminhoPadraoDoDiretorio;
+	private final String caminhoPadraoDoDiretorio;
 	private String caminhoDoDiretorio;
 	private String caminhoDoArquivo;
-	private String sistemaAPI;
+	private final String sistemaAPI;
 	private File arquivo;
-	private Eventv2 eventov2;
+	private final Eventv2 eventov2;
 	private String usuario;
 	private String evento;
-		
+
 	private Escritor escritor;
 	private String exporter;
 	private String wsurl;
-	
-	public TikTak (final String sistema) {
+
+	/**
+	 * Constructor with parameter
+	 * 
+	 * @param system
+	 *            String with name of the system in use
+	 * */
+	public TikTak(final String system) {
 		caminhoPadraoDoDiretorio = System.getProperty("user.dir") + "/";
 		caminhoDoDiretorio = "";
 		caminhoDoArquivo = "";
-		sistemaAPI = sistema;
+		sistemaAPI = system;
 		this.eventov2 = Eventv2.getInstance();
 		this.eventov2.init(sistemaAPI);
 		obterECriarNoDisco();
 	}
-	
-	public TikTak(){
+
+	/**
+	 * Constructor without parameter
+	 * */
+	public TikTak() {
 		this("System");
 	}
 
@@ -48,14 +69,14 @@ public class TikTak {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private String obterCaminhoDoDiretorio() throws IOException {
 		String parametroSetDir, parametroGetProperty, parametroFileProperties;
-		
+
 		parametroFileProperties = obterPropriedadeDoArquivoDeProperties("tiktak.dir");
 		parametroSetDir = this.caminhoDoDiretorio;
 		parametroGetProperty = System.getProperty("tiktak.dir");
-		
+
 		if (parametroSetDir != null && !parametroSetDir.equals("")) {
 			caminhoDoDiretorio = parametroSetDir;
 		} else if (parametroFileProperties != null && !parametroFileProperties.equals("")) {
@@ -64,14 +85,14 @@ public class TikTak {
 			caminhoDoDiretorio = parametroGetProperty;
 		} else {
 			caminhoDoDiretorio = caminhoPadraoDoDiretorio;
-		}		
+		}
 		if (!caminhoDoDiretorio.endsWith("/")) {
 			caminhoDoDiretorio += "/";
 		}
 		return caminhoDoDiretorio;
 	}
 
-	private String obterPropriedadeDoArquivoDeProperties(String propriedade) {
+	private String obterPropriedadeDoArquivoDeProperties(final String propriedade) {
 		String property;
 		File arquivo = new File("tiktak.properties");
 		FileInputStream fis;
@@ -83,35 +104,34 @@ public class TikTak {
 		} catch (Exception e) {
 			property = null;
 		}
-		
+
 		return property;
 	}
-	
+
 	private String criarDiretorioLog() throws IOException {
 		String diretorio = caminhoDoDiretorio;
 		if (diretorio != "") {
-			File diretorioFisico = new File(diretorio);			
+			File diretorioFisico = new File(diretorio);
 			if (!diretorioFisico.exists()) {
 				diretorioFisico.mkdir();
 			}
 		}
 		return diretorio;
 	}
-	
+
 	private String obterCaminhoDoArquivo() throws IOException {
 		String parametroSetArquivo;
-		parametroSetArquivo = "tik.tak";		
-		caminhoDoArquivo = this.caminhoDoDiretorio + parametroSetArquivo;	
+		parametroSetArquivo = "tik.tak";
+		caminhoDoArquivo = this.caminhoDoDiretorio + parametroSetArquivo;
 		exporter = obterPropriedadeDoArquivoDeProperties("tiktak.exporter");
-		if(exporter != null && exporter.equals("webservice")){
+		if (exporter != null && exporter.equals("webservice")) {
 			wsurl = obterPropriedadeDoArquivoDeProperties("tiktak.ws-url");
-		}
-		else{
+		} else {
 			exporter = "file";
-		} 
+		}
 		return caminhoDoArquivo;
 	}
-	
+
 	private File criarArquivoLog() throws IOException {
 		arquivo = new File(this.caminhoDoArquivo);
 		if (!arquivo.exists()) {
@@ -124,38 +144,45 @@ public class TikTak {
 		return arquivo;
 	}
 
-	public void log(final String usuario, final String nomeDoEvento) { 
-		this.evento = nomeDoEvento;
+	/**
+	 * Log the event and the user
+	 * 
+	 * @param user
+	 *            String with name of the user of the system
+	 * @param eventName
+	 *            String with name of the event
+	 * */
+	public void log(final String user, final String eventName) {
+		this.evento = eventName;
 		String json, jsonEventos;
 		try {
-		criarArquivoLog();
+			criarArquivoLog();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			Event evento = new Event(usuario, nomeDoEvento);
+			Event evento = new Event(user, eventName);
 			eventov2.getEvents().add(evento);
 			json = GsonFactory.getGson().toJson(eventov2) + "\n";
 			jsonEventos = GsonFactory.getGson().toJson(evento);
 		}
 		concatenarJson(json, jsonEventos);
 	}
-	
-	public void logTeste(final String usuario, final String nomeDoEvento){
-		
-		if(exporter.equals("webservice")){
+
+	public void logTeste(final String usuario, final String nomeDoEvento) {
+
+		if (exporter.equals("webservice")) {
 			escritor = new EscritorWS();
 			try {
 				escritor.log(usuario, nomeDoEvento, wsurl);
 			} catch (Exception e) {
-				
+
 			}
-		}
-		else{
+		} else {
 			escritor = new EscritorArquivo();
 			try {
 				escritor.log(usuario, nomeDoEvento, caminhoDoArquivo);
 			} catch (Exception e) {
-				
+
 			}
 		}
 	}
@@ -192,19 +219,24 @@ public class TikTak {
 	public String getCaminhoDoArquivo() {
 		return caminhoDoArquivo;
 	}
-	
+
 	public String getCaminhoDoDiretorio() {
 		return caminhoDoDiretorio;
 	}
 
-	public void setDir(String nomeDoDiretorio) {
-		if(nomeDoDiretorio.equals("")){
-			nomeDoDiretorio = caminhoPadraoDoDiretorio;
+	/**
+	 * Set path of directory
+	 * 
+	 * @param nameOfDirectory
+	 *            String with directory name
+	 * */
+	public void setDir(String nameOfDirectory) {
+		if (nameOfDirectory.equals("")) {
+			nameOfDirectory = caminhoPadraoDoDiretorio;
+		} else if (!nameOfDirectory.endsWith("/")) {
+			nameOfDirectory += "/";
 		}
-		else if (!nomeDoDiretorio.endsWith("/")) {
-			nomeDoDiretorio += "/";
-		}
-		this.caminhoDoDiretorio = nomeDoDiretorio;
+		this.caminhoDoDiretorio = nameOfDirectory;
 		obterECriarNoDisco();
 	}
 }
